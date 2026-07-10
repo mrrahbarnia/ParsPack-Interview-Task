155e8dab88d647aad368b62e7f79895ec2f77def
Mohammadreza Rahbarnia <mrrahbarnia@gmail.com>
2026-07-10 16:52:30 +0330

    Change file names from entities to use_cases

--- src/modules/auth/services/__init__.py
+++ src/modules/auth/services/__init__.py
@@ -1,4 +1,4 @@
-from .auth import AuthService
+from .use_cases import AuthService
 from .interfaces import IAuthRepo, IPasswordEncryptor
 
 __all__ = ["AuthService", "IAuthRepo", "IPasswordEncryptor"]
--- src/modules/jobs/services/__init__.py
+++ src/modules/jobs/services/__init__.py
@@ -1,4 +1,4 @@
-from .jobs import JobService
+from .use_cases import JobService
 from .interfaces import IJobRepo
 
 __all__ = ["JobService", "IJobRepo"]
